d7ea0a37f73387b5d2d9a228a8932dc2e12b3aa8
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-08-18 23:29:40 -0400

    + init

---
+++ CMakeFiles/pixel_browser.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pixel_browser.dir/src/main.c.obj"
+  "CMakeFiles/pixel_browser.dir/src/main.c.obj.d"
+  "libpixel_browser.dll.a"
+  "pixel_browser.exe"
+  "pixel_browser.exe.manifest"
+  "pixel_browser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pixel_browser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/pixel_browser.dir/src/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/pixel_browser.dir/src/main.c.obj: \
+ C:\Users\pixil\Desktop\cenv\src\main.c \
+ c:\mingw\x86_64-w64-mingw32\include\stdio.h \
+ c:\mingw\x86_64-w64-mingw32\include\corecrt_stdio_config.h \
+ c:\mingw\x86_64-w64-mingw32\include\corecrt.h \
+ c:\mingw\x86_64-w64-mingw32\include\_mingw.h \
+ c:\mingw\x86_64-w64-mingw32\include\_mingw_mac.h \
+ c:\mingw\x86_64-w64-mingw32\include\_mingw_secapi.h \
+ c:\mingw\x86_64-w64-mingw32\include\vadefs.h \
+ c:\mingw\x86_64-w64-mingw32\include\sdks/_mingw_ddk.h \
+ c:\mingw\x86_64-w64-mingw32\include\_mingw_off_t.h \
+ c:\mingw\x86_64-w64-mingw32\include\swprintf.inl \
+ c:\mingw\x86_64-w64-mingw32\include\sec_api\stdio_s.h
